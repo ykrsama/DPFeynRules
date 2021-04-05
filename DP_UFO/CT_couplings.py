578d029295f19c29788d14f3b4b737dca6b48418
zhuxu <zhuxuliang@outlook.com>
2021-04-05 18:33:54 +0800

    v0.1

---
+++ DP_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.41
+# Mathematica version: 12.1.0 for Microsoft Windows (64-bit) (March 14, 2020)
+# Date: Mon 5 Apr 2021 18:27:30
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ DP_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.41
+# Mathematica version: 12.1.0 for Microsoft Windows (64-bit) (March 14, 2020)
+# Date: Mon 5 Apr 2021 18:27:30
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
