886b01abd6437df99c1331daa6c23bff47b64e15
Xuliang Zhu <xuliang.zhu@cern.ch>
2023-11-01 12:03:28 +0800

    noGin

--- DP_UFO/form_factors.py
+++ DP_UFO/form_factors.py
@@ -2,12 +2,12 @@ from object_library import all_form_factors, FormFactor
 
 from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
 
-Gel = '((anuc**2 * _t_) / (1 + anuc**2 * _t_))**2 * (1 / (1 + _t_/dnuc))**2 * Znuc**2'
+Gel = '((anuc**2 * _t_) / (1 + anuc**2 * _t_) / (1 + _t_/dnuc) )**2'
 
-Gin = '((apnuc**2 * _t_) / (1 + apnuc**2 * _t_))**2 * (1 + _t_ * (mupnuc**2 - 1) / (4 * mpnuc**2))  / (1 + _t_ / 0.71**2)**4 * Znuc'
+Gin = '(apnuc**2 * _t_ * (1 + _t_ * (mupnuc**2 - 1) / (4 * mpnuc**2)) / (1 + apnuc**2 * _t_) / (1 + _t_ / 0.71)**4 )**2 / Znuc'
 
 FF = FormFactor(name = 'FF',
                 type = 'real',
-                value= ('sqrt(' + Gel + '+' + Gin + ')').replace('_t_','(P(-1,1)-P(-1,2))**2'))
+                value= ('sqrt(' + Gel + ')').replace('_t_','(P(-1,1)-P(-1,2))**2'))
 
 
