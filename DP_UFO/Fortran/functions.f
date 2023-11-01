58a15f66c009e86d01236cc5c61a3c0e66aa55b0
Xuliang Zhu <xuliang.zhu@cern.ch>
2023-11-02 01:09:45 +0800

    FormFactor

--- DP_UFO/Fortran/functions.f
+++ DP_UFO/Fortran/functions.f
@@ -1,7 +1,15 @@
-double complex function FormFactor(S1)
-   double complex S1, Gel
-   include 'input.inc' ! include all model parameter
-   Gel = ((anuc**2 * abs(S1)) / (1 + anuc**2 * abs(S1)) / (1 + abs(S1)/dnuc) )**2
-   FormFactor = SQRT(Gel) 
-   return 
+double precision function mymdl_FormFactor(t)
+    implicit NONE
+    include 'input.inc' ! include all model parameter
+    double precision t
+    double precision Gel
+    double precision OUTVAL
+    Gel = (mdl_anuc**2 * t / (1 + mdl_anuc**2 * t) / (1 + t / mdl_dnuc) )**2
+    mymdl_FormFactor = dsqrt(Gel) 
+    !mymdl_FormFactor = 1.0 ! for no FF case
+    OUTVAL = mymdl_FormFactor
+    PRINT *, 't:', t
+    PRINT *, 'Form Factor:', OUTVAL
+    
+    return 
 end
